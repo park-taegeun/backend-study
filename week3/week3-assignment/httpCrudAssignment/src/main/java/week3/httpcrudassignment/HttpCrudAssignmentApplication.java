2bdc04490a498b3cc4bdfd23c5bd312f27212183
박태근 <taegeun1234@naver.com>
2026-02-16 16:40:22 +0900

    feat: week3 http crud assignment project set up

---
+++ week3/week3-assignment/httpCrudAssignment/src/main/java/week3/httpcrudassignment/HttpCrudAssignmentApplication.java
@@ -0,0 +1,13 @@
+package week3.httpcrudassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpCrudAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpCrudAssignmentApplication.class, args);
+    }
+
+}
